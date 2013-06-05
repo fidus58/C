548fb8ff35e32b3e3015bf646621e7cad1f5f072
Klaus Ahrens <ahrens@informatik.hu-berlin.de>
2013-06-05 09:14:58 +0200

    Initial Commit

---
+++ vectorMaxSize/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  vectorMaxSize
+//
+//  Created by Klaus Ahrens on 05.06.13.
+//  Copyright (c) 2013 Klaus Ahrens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
